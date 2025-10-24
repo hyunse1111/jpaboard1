7a5fbfddbeb7d3ddc494992eedcf34e383630c9e
DESKTOP-69TFTE7\it <hynk8563@gmail.com>
2025-10-24 15:13:02 +0900

    jpaboard version 2

--- src/main/java/org/pgm/jpademo/domain/Reply.java
+++ src/main/java/org/pgm/jpademo/domain/Reply.java
@@ -10,6 +10,7 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Reply extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
